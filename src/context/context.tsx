dbb2b4f991ed9c8b4b13eb40fe149d55b81459ef
Roger Bentcha <remirjunior1999@gmail.com>
2024-09-08 14:00:59 +0200

    Fix: Localstorage n'est pas définit

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,63 +1,76 @@
 "use client";
 
-import React, {ReactNode} from "react";
+import React, { ReactNode } from "react";
 
 interface AppContextProps {
-  tasks: Task[]
-  deleteAll: () => void
-  add: (task: Task) => void
-  setCompleted: (id: number, value: boolean) => void
+  tasks: Task[];
+  deleteAll: () => void;
+  add: (task: Task) => void;
+  setCompleted: (id: number, value: boolean) => void;
 }
 
 const AppContext = React.createContext<AppContextProps>({
   tasks: [],
   deleteAll: () => null,
   add: () => null,
-  setCompleted: (id) => null
-})
+  setCompleted: (id) => null,
+});
 
-const AppProvider = ({children}: { children: ReactNode }) => {
-  const localTasksString = localStorage.getItem('app-tasks');
-  const localTasks = localTasksString ? JSON.parse(localTasksString) : []
-  const [tasks, setTasks] = React.useState<Task[]>([...localTasks, {
-    id: 1,
-    description: 'Préparer une présentation pour la réunion de lundi',
-    completed: false,
-    createAt: Date.now()
-  }]);
+const AppProvider = ({ children }: { children: ReactNode }) => {
+  const [localTasks, setLocalTasks] = React.useState("");
+  const [tasks, setTasks] = React.useState<Task[]>([
+    {
+      id: 1,
+      description: "Préparer une présentation pour la réunion de lundi",
+      completed: false,
+      createAt: Date.now(),
+    },
+  ]);
+
+  React.useEffect(() => {
+    const localTasksString = window.localStorage.getItem("app-tasks");
+
+    if (localTasksString) {
+      const t: Task[] = JSON.parse(localTasksString);
+      if (t) {
+        setTasks((prev) => [...tasks, ...t]);
+      }
+    }
+  }, []);
 
   const deleteAll = () => {
     setTasks([]);
-    window.localStorage.setItem('app-tasks', "");
-  }
+    window.localStorage.setItem("app-tasks", "");
+  };
 
   const update = (t: Task[]) => {
     setTasks(t);
-    localStorage.setItem('app-tasks', JSON.stringify(t))
-  }
+    localStorage.setItem("app-tasks", JSON.stringify(t));
+  };
 
   const add = (task: Task) => {
-    const t = [...tasks, task]
-    update(t)
-  }
+    const t = [...tasks, task];
+    update(t);
+  };
 
   const setCompleted = (id: number, value: boolean) => {
-    const updatedTasks = tasks.map(task => {
+    const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
-        task.completed = value
+        task.completed = value;
       }
-      return task
-    })
+      return task;
+    });
 
-    console.log(updatedTasks)
+    console.log(updatedTasks);
 
-    update(updatedTasks)
-  }
+    update(updatedTasks);
+  };
 
+  return (
+    <AppContext.Provider value={{ tasks, deleteAll, add, setCompleted }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-  return <AppContext.Provider value={{tasks, deleteAll, add, setCompleted}}>
-    {children}
-  </AppContext.Provider>
-}
-
-export {AppContext, AppProvider}
+export { AppContext, AppProvider };
